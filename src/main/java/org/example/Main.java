257b31ee197f997dfd80ee3eade9ea9619216f8b
Ramazan ÖZKURT <ramazanozkurt@gmail.com>
2022-12-17 17:18:06 +0300

    Singleton Example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+package org.example;
+
+import org.example.service.SingletonService;
+
+public class Main {
+  public static void main(String[] args) {
+
+    SingletonService singletonService = SingletonService.getInstance();
+
+    SingletonService singletonService1 = SingletonService.getInstance();
+
+    System.out.println(singletonService == singletonService1); // expected true
+
+
+    singletonService.setName("BMW");
+
+    System.out.println(singletonService.getName()); // expected BMW  / printed singletonService::getName
+
+    singletonService1.setName("MERCEDES");  // singletonService1::setName
+
+    System.out.println(singletonService.getName()); // expected "Mercedes" BMW  / printed singletonService::getName
+
+    System.out.println(singletonService.hashCode()); //expected singletonService1::hashCode == singletonService1::hashCode
+    System.out.println(singletonService1.hashCode());
+
+
+  }
 }
--- src/main/java/org/example/service/SingletonService.java
+++ src/main/java/org/example/service/SingletonService.java
@@ -1,2 +1,25 @@
-package org.example.service;public class SingletonService {
+package org.example.service;
+
+public class SingletonService {
+
+  private String name;
+
+  private static SingletonService instance;
+
+  private SingletonService(){}
+
+  public static SingletonService getInstance() {
+    if(instance == null) {
+      instance = new SingletonService();
+    }
+    return instance;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
